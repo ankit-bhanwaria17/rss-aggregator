a8cd5947dfbf2c72728191206d08d71b793db31c
Ankit Bhanwaria <ankit.bhanwaria17@gmail.com>
2023-09-03 14:25:59 +0530

    get github/joho/godotenv package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ankit-bhanwaria17/rss-aggregator
 
 go 1.21.0
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello")
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT variable missing from environment")
+	}
+
+	fmt.Println("Running at PORT: ", portString)
 }
